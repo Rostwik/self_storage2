36428cd49e3c10e8d587b807d7295cc66ee86e5b
Rostislav <rostwik@gmail.com>
2021-11-29 01:40:09 +0300

    Heroku start

--- self_storage/self_storage/settings.py
+++ self_storage/self_storage/settings.py
@@ -98,15 +98,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
-USE_L10N = True
-
-USE_TZ = True
+USE_L10N = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
